8103a5b2800e579c7fb2c6bffc4ad9e1f2af882d
Taku-Sashima <131934396+Taku-Sashima@users.noreply.github.com>
2025-10-23 18:10:01 +0900

    変更

--- abc-rust/src/abc409/c.rs
+++ abc-rust/src/abc409/c.rs
@@ -1,53 +1,5 @@
-// use proconio::input;
-// // use std::collections::*;
-
-// fn main(){
-//     input!{
-//         n : usize,
-//         l : usize,
-//         a : [usize; n-1],
-//     }
-
-//     // 円周が3の倍数出ない時は、絶対に正三角形が作れない。（入力が整数のため）
-//     if l%3 != 0{
-//         print!("{}", 0);
-//         std::process::exit(0);
-//     }
-//     let distance = l/3;
-//     // println!("{}", distance);
-
-//     let mut count = vec![0; l];
-
-//     count[0] = 1;
-
-//     let mut current_position = 0;
-//     for i in a{
-//         current_position = (current_position+i)%l;
-//         count[current_position] += 1;
-//     }
-
-//     // println!("{:?}", position_map);
-
-//     let mut ans:usize = 0;
-
-//     for i in 0..distance{
-//         let c1 = count[i];
-//         let c2 = count[i + distance];
-//         let c3 = count[i + distance*2];
-
-//         ans += c1 * c2 * c3;
-//         // println!("i : {} {}", i ,ans);
-//     }
-
-//     println!("{}", ans);
-
-
-
-// }
-
-
 use proconio::input;
-use std::collections::*;
+// use std::collections::*;
 
 fn main(){
     input!{
@@ -56,35 +8,26 @@ fn main(){
         a : [usize; n-1],
     }
 
-    // 円周が3の倍数出ない時は、絶対に正三角形が作れない。（入力が整数のため）
     if l%3 != 0{
         print!("{}", 0);
         std::process::exit(0);
     }
     let distance = l/3;
-    // println!("{}", distance);
 
-    let mut position_map = BTreeMap::new();
-    for i in 0..l{
-        position_map.insert(i, 0 as usize);
-    }
-    position_map.insert(0, 1);
+    let mut count = vec![0; l as usize];
+    count[0] = 1;
 
     let mut current_position = 0;
     for i in a{
         current_position = (current_position+i)%l;
-        let count = position_map.entry(current_position).or_insert(0);
-        *count += 1;
+        count[current_position as usize] += 1;
     }
 
-    // println!("{:?}", position_map);
-
-    let mut ans = 0;
-
+    let mut ans:usize = 0;
     for i in 0..distance{
-        let c1 = *position_map.get(&i).unwrap_or(&0);
-        let c2 = *position_map.get(&(i+distance)).unwrap_or(&0);
-        let c3 = *position_map.get(&((i+distance*2)%l)).unwrap_or(&0);
+        let c1 = count[i as usize];
+        let c2 = count[(i + distance) as usize];
+        let c3 = count[(i + distance*2) as usize];
 
         ans += c1 * c2 * c3;
         // println!("i : {} {}", i ,ans);

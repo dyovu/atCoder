84d9d6593a0a6d4bf9bc0370fc897ea070cf6e3a
dyovu <131934396+dyovu@users.noreply.github.com>
2026-02-26 14:52:12 +0900

    2026-2-26

--- abc-rust-414~/abc440/src/bin/c.rs
+++ abc-rust-414~/abc440/src/bin/c.rs
@@ -18,6 +18,39 @@ fn main(){
         let w = situation[1];
         let c:Vec<usize> = lines.next().unwrap().unwrap().split_whitespace().map(|x| x.parse::<usize>().unwrap()).collect();
 
+        if n <= w {
+            println!("{}", 0);
+            continue
+        }
+
+        let ww = 2*w;
+
+        let mut cost_per_rem:Vec<usize> = vec![0; ww];
+        for (index, cost) in c.iter().enumerate(){
+            cost_per_rem[index%ww] += cost;
+        }
+
+        // println!("{:?}", cost_per_rem);
+
+        let partial_sum_num = w;
+
+
+        // println!("partial_sum_num: {:?}", partial_sum_num);
+
+        let mut partial_sum:usize = cost_per_rem[0..partial_sum_num].iter().sum();
+        // println!("partial_sum: {}", partial_sum);
+        let mut partial_sum_min:usize = partial_sum;
+
+        for index in 0..ww{
+            partial_sum = partial_sum - cost_per_rem[index] + cost_per_rem[(index+partial_sum_num)%ww];
+            if partial_sum < partial_sum_min{
+                partial_sum_min = partial_sum;
+            }
+        }
+
+        println!("{}", partial_sum_min);
+
+
     }
 
 

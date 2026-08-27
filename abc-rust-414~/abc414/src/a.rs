035995bbd6181317cc77343eada8933514c2dcc0
dyovu <131934396+dyovu@users.noreply.github.com>
2026-08-27 14:17:48 +0900

    abc414: add A solution

--- abc-rust-414~/abc414/src/a.rs
+++ abc-rust-414~/abc414/src/a.rs
@@ -26,3 +26,29 @@ pub fn main_a(){
 
 }
 
+use proconio::input;
+// use std::{collections::{BTreeMap, HashMap}, io::{self, BufRead}};
+use std::collections::*;
+
+fn print_type<T>(_: T) {
+    println!("{}", std::any::type_name::<T>());
+}
+
+fn main(){
+    input!{
+        s: String,
+    }
+    
+   let mut map = BTreeMap::new();
+
+    for i in s.chars(){
+       *map.entry(i).or_insert(0) += 1;
+    }
+    let (_, max) = map.iter().max().unwrap();
+    for i in s.chars(){
+        if map.get(&i).unwrap() != max{
+            print!("{}", i);
+        }
+    }
+}
+
